48f2690963876bbd7fdc4971f081da68ac622eca
Jon Ashcroft <hello@jonashcroft.co.uk>
2018-11-18 18:05:24 +0000

    Test

--- source/js/helpers/config.js
+++ source/js/helpers/config.js
@@ -1,7 +1,6 @@
 const config = {
     'clientId'     : '*** CLIENT ID ***',
     'clientSecret' : '*** CLIENT SECRET ***',
-
     'redirectUrl'  : 'http://localhost:8080',
     'monzoUrl'     : 'https://api.monzo.com',
 }
