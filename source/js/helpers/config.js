4c6b5c2fbf027bc1040c56a42efa3abcc7068fc8
Jon Ashcroft <hello@jonashcroft.co.uk>
2019-01-05 20:39:58 +0000

    fixed untracked files

--- source/js/helpers/config.js
+++
@@ -1,8 +0,0 @@
-const config = {
-    'clientId'     : '*** CLIENT ID ***',
-    'clientSecret' : '*** CLIENT SECRET ***',
-    'redirectUrl'  : 'http://localhost:8080',
-    'monzoUrl'     : 'https://api.monzo.com',
-}
-
-export default config;
