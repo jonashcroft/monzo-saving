4ac33f61dd129ef78c2541bb3f8d23097e9ecde8
Jon Ashcroft <hello@jonashcroft.co.uk>
2019-01-05 22:00:00 +0000

    Add: Axios to get auth code / token

--- source/js/auth/authorise.js
+++ source/js/auth/authorise.js
@@ -1,8 +1,9 @@
+import config from '../helpers/config'
+import axios from 'axios'
+
 class Authorise {
     constructor (elem) {
        this._elem = elem
-       this._clientIdInput = this._elem.querySelector('[data-auth-client-id]')
-       this._clientSecretInput = this._elem.querySelector('[data-auth-client-secret]')
        this._authBtn = this._elem.querySelector('[data-auth-btn]')
     }
 
@@ -13,22 +14,14 @@ class Authorise {
         * app/auth based on results from db
         */
 
-       let accessToken = new URLSearchParams(document.location.search).get('code')
+       let accessCode = new URLSearchParams(document.location.search).get('code')
 
-        if ( accessToken ) {
-           this._getAccessToken( accessToken );
+        if ( accessCode ) {
+           this._getAccessToken( accessCode )
         }
-
-        let clientId
-        let clientSecret
 
         this._authBtn.addEventListener('click', e => {
             e.preventDefault()
-
-            clientId = this._clientIdInput.value
-            clientSecret = this._clientSecretInput.value
-
-            console.log(`Client ID: ${clientId}`, `Client Secret: ${clientSecret}`)
 
             this._initAuth()
         })
@@ -42,14 +35,47 @@ class Authorise {
 
         let authUrl = `https://auth.monzo.com/?client_id=${ config.clientId }&redirect_uri=${ config.redirectUrl }&response_type=code&state=${ Math.random().toString(23).substring(4) }`;
 
-        window.location.replace(authUrl);
+        window.location.replace(authUrl)
 
     }
 
-    _getAccessToken ( accessToken ) {
+    _getAccessToken ( accessCode ) {
+        console.dir(`Access Code: ${accessCode}`)
         /**
          * Axios request to Monzo
          */
+
+        let formData = new FormData()
+
+        formData.append('client_id', config.clientId);
+        formData.append('client_secret', config.clientSecret);
+        formData.append('code', accessCode);
+        formData.append('grant_type', 'authorization_code');
+        formData.append('redirect_uri', config.redirectUrl);
+
+        const url = `${config.monzoUrl}/oauth2/token`;
+
+        axios({
+            method: 'post',
+            url: url,
+            data: formData,
+            config: { headers: {'Content-Type': 'multipart/form-data' }}
+            })
+            .then(function (response) {
+                //handle success
+                console.log(response)
+                console.log(response.data)
+                console.log(response.data.access_token)
+                console.log(response.data.refresh_token)
+
+                // sessionStorage.setItem('accessToken', authResponse.access_token);
+                // sessionStorage.setItem('refreshToken', authResponse.refresh_token);
+
+            })
+            .catch(function (response) {
+                //handle error
+                console.log(response);
+            })
     }
 }
 
--- source/js/main.js
+++ source/js/main.js
@@ -2,25 +2,6 @@ import Authorise from './auth/authorise'
 import Timer from './components/timer'
 import Test from './components/test'
 
-import firebase from 'firebase/app';
-import * as firebaseui from 'firebaseui'
-
-// https://firebase.google.com/docs/auth/web/firebaseui
-// https://github.com/firebase/firebaseui-web#demo
-
-var config = {
-    apiKey: "AIzaSyBtPMqe1nAPsJw1zqLbwo4ZZYSbCFMjf1w",
-    authDomain: "monzo-app-181a0.firebaseapp.com",
-    databaseURL: "https://monzo-app-181a0.firebaseio.com",
-    projectId: "monzo-app-181a0",
-    storageBucket: "monzo-app-181a0.appspot.com",
-    messagingSenderId: "19214649606"
-}
-
-firebase.initializeApp(config);
-
-console.dir(firebase)
-
 const components = {
     'authorise': Authorise,
     'test': Test,
